68c9e8ad36bdb73d7b77b6c6cd9ca67925721c7a
Eduardo Serafin <e.serafin@ed.ac.uk>
2017-03-10 23:25:30 +0000

    first full comit

---
+++ app/controllers/greeter_controller.rb
@@ -0,0 +1,16 @@
+class GreeterController < ApplicationController
+  def hello
+  random_names = ["Alex", "Joe", "Michael"]
+  @name = random_names.sample
+    @time = Time.now
+    @times_displayed ||=0
+    @times_displayed += 1
+  end
+  def goodbye
+    random_names = ["Alex", "Joe", "Michael"]
+    @name = random_names.sample
+    @time = Time.now
+    @times_displayed ||=0
+    @times_displayed += 1
+  end
+end
